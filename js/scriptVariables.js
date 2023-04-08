f9f9681a863568df3af9ca05816b5bc2b11d99b9
Nathaniel Soriano <nathaniel.valdez.soriano@gmail.com>
2023-04-07 21:29:59 -0400

    Export from Replit ZIP
    All tasks for the homework included.

---
+++ js/scriptVariables.js
@@ -0,0 +1,14 @@
+// Working with Variables
+let admin, name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+
+// Giving the right name
+const ourPlanetName = "Earth";
+
+let currentUser = [];
